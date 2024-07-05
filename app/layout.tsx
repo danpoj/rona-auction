c1814b394a2c13979dee464a836e64de489a0a94
danpoj <jws970306@khu.ac.kr>
2024-07-05 13:17:35 +0900

    fix Minified Error #425

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,6 @@ const inter = Poppins({ subsets: ['latin'], weight: ['400', '600', '900'] });
 export const metadata: Metadata = {
   title: '로나월드 옥션',
   description: '로나월드 옥션 거래내역 저장소입니다.',
-  verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-  },
 };
 
 export default function RootLayout({
@@ -28,13 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <head>
-        <AdsenseScript />
-        <meta
+      <AdsenseScript />
+
+      {/* <meta
           name='google-adsense-account'
           content={`ca-pub-${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_PID}`}
-        ></meta>
-      </head>
+        ></meta> */}
+
       <body className={cn(inter.className, 'overscroll-none')}>
         <BackgroundPattern />
         <QueryProvider>
