ddbd3a14df0deb807be57a0c8b7a6b6897a2ebca
danpoj <jws970306@khu.ac.kr>
2024-07-08 11:45:25 +0900

    add robots

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -28,7 +28,7 @@ export default function Page() {
 
       <div className='space-y-20'>
         <article className='prose dark:prose-invert'>
-          <h1>7/5(금) 업데이트</h1>
+          <h1>7/7(일) 업데이트</h1>
           <ul>
             <li>장비아이템 보기 옵션 (리스트, 그리드) 추가</li>
             <li>검색바 유지</li>
---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://www.ronaoff.com/sitemap.xml',
+    host: 'https://www.ronaoff.com',
+  };
+}
